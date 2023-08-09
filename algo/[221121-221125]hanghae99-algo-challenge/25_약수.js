1211f6c04310e66970ee377dba063fb3b1b4c9ac
dan <dan@payapcorp.com>
2023-08-09 19:14:50 +0900

    :tada: Push all

--- algo/[221121-221125]hanghae99-algo-challenge/25_약수.js
+++
@@ -1,9 +0,0 @@
-function solution(left, right) {
-    var answer = 0;
-        for(let i=left; i<=right; i++){
-            let sqrt = Math.sqrt(i)
-            sqrt%1 ===0 ? answer-=i : answer+=i
-        }
-    return answer;
-}
-console.log(solution(24,27))
